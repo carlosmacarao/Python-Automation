6fbacd3c3304eb0ddfec2b9e3f90dc7905bbe6b2
Carlos Macarao <carlosmacarao9@gmail.com>
2025-01-29 20:58:21 +0200

    new update

--- siga.py
+++ siga.py
@@ -1,9 +1,36 @@
 import pyautogui 
+import time
 
-pyautogui.PAUSE = 1
+pyautogui.PAUSE = 0.8
 
 pyautogui.press("win")
 
 pyautogui.write("chrome")
+
+pyautogui.press("enter")
+
+pyautogui.write("https://sigauz.unizambeze.ac.mz/login.zul")
+
+pyautogui.press("enter")
+
+time.sleep(2)
+
+pyautogui.press("tab")
+
+pyautogui.write("20204061")
+
+pyautogui.press("tab")
+
+pyautogui.write("Carlos123#")
+
+pyautogui.press("tab")
+pyautogui.press("tab")
+
+pyautogui.press("enter")
+
+time.sleep(2)
+
+pyautogui.press("tab")
+pyautogui.press("tab")
 
 pyautogui.press("enter")
